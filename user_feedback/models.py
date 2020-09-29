115226fc69afaa3135cee38b8cb3e65201da06bf
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2020-09-28 23:41:00 -0400

    Update admin and add archive field to Feedback model

--- user_feedback/models.py
+++ user_feedback/models.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext as _
 
 User = get_user_model()
 
 
 class Feedback(models.Model):
 
-    ONE_TO_FIVE_RATING_CHOICES = ((1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5"))
     FEEDBACK_TYPE = ((1, "Bug Report"), (2, "Feature Request"), (3, "General Feedback"))
 
     type = models.PositiveIntegerField(choices=FEEDBACK_TYPE)
@@ -14,6 +14,11 @@ class Feedback(models.Model):
     url = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    archived = models.BooleanField(
+        help_text=_("Check to mark this comment as resolved."),
+        verbose_name=_("archive status"),
+        default=False,
+    )
 
     def __str__(self):
         return (
